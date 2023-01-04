a160561487d5b027b86052a64e93e919960ba603
akarki7 <karkiaabishkar@gmail.com>
2023-01-04 19:16:50 +0545

    Changed initial structure of website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import LoginForm from './components/Login';
+import "./index.css";
+
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        bg: "#f9f9f9",
+      },
+    }),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <LoginForm />
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    Container,
+    Image,
+    Flex,
+    Heading,
+    Text,
+    Stack,
+    Box,
+    Center,
+} from '@chakra-ui/react';
+
+const LoginForm = () => {
+    return (
+        <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
+            <Center>
+                <Container>
+                    <Box>
+
+                    </Box>
+                </Container>
+            </Center>
+        </Flex>
+    );
+};
+
+export default LoginForm;

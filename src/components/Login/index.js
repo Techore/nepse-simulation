68005de9432dab3fbb2303456fec8c2111c2a65e
akarki7 <karkiaabishkar@gmail.com>
2023-01-04 20:41:21 +0545

    First row finished along with mobile version

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,24 +3,55 @@ import {
     Container,
     Image,
     Flex,
-    Heading,
     Text,
     Stack,
     Box,
     Center,
+    HStack,
+    Divider,
+    VStack,
 } from '@chakra-ui/react';
+
+import NEPSE_LOGO from './images/NEPSE.png'
 
 const LoginForm = () => {
     return (
         <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
             <Center>
-                <Container>
-                    <Box>
-
-                    </Box>
-                </Container>
+                <Box
+                    bgColor={"white"}
+                    boxShadow='xl'
+                    rounded='xl'
+                    p={{ base: 5, sm: 5, md: 5, lg: 6, xl: 6 }}
+                    m={{ base: 4, sm: 4, md: 16, lg: 25, xl: 25 }}
+                    mb={'15px'}
+                    minHeight="85vh"
+                    minWidth={{ lg: 600, xl: 600 }}
+                >
+                    <Stack direction={"row"} h='100px'>
+                        <Box>
+                            <Image
+                                alt={'Techore Solutions'}
+                                borderRadius='full'
+                                objectFit='cover'
+                                align={'center'}
+                                boxSize={{ base: '80px', lg: '120px', xl: '120px' }}
+                                src={NEPSE_LOGO}
+                                mr={5}
+                                id="main-logo"
+                            />
+                        </Box>
+                        <Divider orientation='vertical' borderColor={"grey"} />
+                        <Center>
+                            <VStack ml={{ base: 2, xl: 5 }}>
+                                <Text fontWeight={"bold"} color={"#0078d7"} fontSize={{ base: 14, lg: 25, xl: 25 }}>NEPAL STOCK EXCHANGE</Text>
+                                <Text fontWeight={"bold"} color={"#0078d7"} fontSize={{ base: 10, lg: 20, xl: 20 }}>VIRTUAL TRADING SYSTEM</Text>
+                            </VStack>
+                        </Center>
+                    </Stack>
+                </Box>
             </Center>
-        </Flex>
+        </Flex >
     );
 };
 
